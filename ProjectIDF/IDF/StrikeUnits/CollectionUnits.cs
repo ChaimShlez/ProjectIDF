ef937dd91c0690a4d7145c8d5c75961526c84c9a
shmuelgross <shmuelgross3@gmail.com>
2025-05-21 16:05:44 +0300

    create system shmuel

---
+++ ProjectIDF/IDF/StrikeUnits/CollectionUnits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectIDF.Base;
+
+namespace ProjectIDF.IDF.StrikeUnits
+{
+    internal class CollectionUnits
+    {
+        List<AttackUnits> units;
+
+        public CollectionUnits()
+        {
+            units = new List<AttackUnits>();
+        }
+
+        public List <AttackUnits> MyCollectionUnits
+        {
+            get { return units; }
+            set { units = value; }
+        }
+    }
+}
