855312f9b689963f9fdf85edb3d42a5485d9458b
shmuelgross <shmuelgross3@gmail.com>
2025-05-22 12:27:17 +0300

    function Ready for attack

---
+++ ProjectIDF/IDF/Aman/IntelligenceAnalysis/AttackAvailability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectIDF.Base;
+
+namespace ProjectIDF.IDF.Aman.IntelligenceAnalysis
+{
+    internal class AttackAvailability
+    {
+        public List<AttackUnits> ReadyAttack(List<AttackUnits> units) 
+        {
+            List<AttackUnits> unitsList = new List<AttackUnits>();
+            foreach (AttackUnits unit in units)
+            {
+                if (unit.MyAmmunitionCapacity > 0)
+                {
+                    unitsList.Add(unit);
+                }
+            }
+            return unitsList;
+        }
+    }
+}
